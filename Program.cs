81b0cddc6854b6cf3cb4be1bb6ac95a144b79f73
Matt Powell <MattP598@gmail.com>
2020-11-11 00:31:10 -0600

    New update

--- Program.cs
+++ Program.cs
@@ -2,11 +2,18 @@
 
 namespace Average
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter first number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter third number: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int average = (x + y + z) / 3;
+            Console.WriteLine("The average of the three numbers is " + average);
         }
     }
 }
